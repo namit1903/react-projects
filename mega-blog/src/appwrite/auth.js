ca75dc852dd6d394cc7dbe0f5efbc7c12f89c1d8
Thakur <mrhimachal1903@gmail.com>
2024-05-11 13:05:30 +0530

    auth service setup using appwrite

---
+++ mega-blog/src/appwrite/auth.js
@@ -0,0 +1,61 @@
+import conf from '../conf.js';
+
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService{
+  client=new Client();
+  account;
+  constructor(){
+    this.client
+    .setEndpoint(conf.appwriteUrl)
+    .setProject(conf.appwriteProject);
+    this.account=new Account();
+    
+  }
+
+  async createAccount({email,password,name}){
+  try{
+    const userAccount=  await this.account.create(ID.unique(),email,password,name);
+    if(userAccount){
+      //call another function
+      return this.login({email,password});
+      
+    }
+    else{
+      return        
+    }}
+    catch(error){
+      throw error;
+        }
+  }
+  async login({email, password}){
+    try{
+    await this.account.createEmailSesssion(email,password);
+  }  catch(error){
+    throw error;
+      }
+
+    }
+  async getCurrentUser(){
+    try{
+     return await this.account.get();
+    }
+    catch(error){
+      console.log("appwrite service ke ander error hai");
+throw error;
+    }
+    return null;  
+  }
+  async logout(){
+    try{
+    await this.account.deleteSessions('current');
+  }  catch(error){
+    throw error;
+      }
+
+    }
+  }
+
+  const authService = new AuthService();
+
+export default AuthService;
