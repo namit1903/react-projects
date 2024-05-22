24d102d2884e50878a3634668430a599b5a1d77f
Thakur <mrhimachal1903@gmail.com>
2024-05-22 19:31:32 +0530

    blog app completed

---
+++ blog-app/src/appwrite/auth.js
@@ -0,0 +1,69 @@
+import conf from '../conf/conf.js';
+import { Client, Account, ID } from "appwrite";
+
+//class bna li Authservice ki
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+            
+        // Debug log to check available methods on account
+        console.log("Account methods:", Object.keys(this.account));
+    }
+    
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // call another method
+                return this.login({email, password});
+            } else {
+               return  userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            if (typeof (this.account.createEmailSession )!== 'function') {
+                throw new Error('yee createEmailSession is not a function');
+            }
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService
+
+
